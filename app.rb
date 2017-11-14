3f37ca360b8291711b0e98e1acd749284fbfcfa2
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-14 19:05:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,12 +10,10 @@ class App < Sinatra::Base
       erb :newteam
     end
 
-
-
     post '/team' do
       params.inspect
       @team = params
-
+#send info and expecting something to change. you send params 
       # puts params # to see in console
       # team = params[ "what's in here"]
       # #how do i pass the data to the team.erb template?
