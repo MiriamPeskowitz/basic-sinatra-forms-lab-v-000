1b63aaac31b9cc45807e0cb300dabb0805759981
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-14 18:38:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,13 +4,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+
     get '/newteam' do
       erb :newteam
     end
 
     post '/team' do
-      puts params
-      
+      params.inspect
+      # puts params # to see in console 
+      # team = params[ "what's in here"]
+      # #how do i pass the data to the team.erb template? 
+      # team.each {|t| @t}
+      #  t.
       erb :team
     end
 end
+ 
